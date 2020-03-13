32e88ff5d7a9348c0d2e31263a52a0f0952e6590
sravary <sravary@hotmail.com>
2020-03-13 09:40:19 -0400

    Initialized JS file

---
+++ script.js
@@ -0,0 +1,14 @@
+const targetDate = moment("1/1/2021");
+
+const $days = $("#days"),
+  $hours = $("#hours"),
+  $minutes = $("#minutes"),
+  $seconds = $("#seconds");
+
+setInterval(function() {
+  //Calculate the time remaining
+}, 10);
+
+function updateClock(remainingTime) {
+  //Display the difference
+}
